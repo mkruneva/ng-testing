cc93af25d40c6698ad93a087ea21e8ffede61e7a
mkruneva <marta.kruneva@gmail.com>
2018-02-07 11:33:37 +0000

    pipe unit test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { Ex1TextSummaryPipeComponent } from './ex-1-text-summary-pipe/ex-1-text-summary-pipe.component';
-import { Ex2LikeComponent } from './ex-2-like/ex-2-like.component';
-import { Ex3VoterComponent } from './ex-3-voter/ex-3-voter.component';
-import { Ex4UserComponent } from './ex-4-user/ex-4-user.component';
+import { TextSummaryPipe } from './ex-1-text-summary-pipe/ex-1-text-summary-pipe.component';
+import { LikeComponent } from './ex-2-like/ex-2-like.component';
+import { VoterComponent } from './ex-3-voter/ex-3-voter.component';
+import { UsersComponent } from './ex-4-user/ex-4-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Ex1TextSummaryPipeComponent,
-    Ex2LikeComponent,
-    Ex3VoterComponent,
-    Ex4UserComponent
+    TextSummaryPipe,
+    LikeComponent,
+    VoterComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
--- src/app/ex-1-text-summary-pipe/ex-1-text-summary-pipe.component.spec.ts
+++ src/app/ex-1-text-summary-pipe/ex-1-text-summary-pipe.component.spec.ts
@@ -1,7 +1,7 @@
 
 import { TextSummaryPipe } from './ex-1-text-summary-pipe.component';
 
-describe('TextSummaryPipe Component', () => {
+fdescribe('TextSummaryPipe Component', () => {
   let component: TextSummaryPipe;
 
   beforeEach(() => {
@@ -10,5 +10,25 @@ describe('TextSummaryPipe Component', () => {
 
   it('should create TextSummaryPipe Component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return an empty string if input is null', () => {
+    expect (component.transform(null)).toEqual('');
+  });
+
+  it('should return null if input is undefined', () => {
+    expect (component.transform(undefined)).toEqual('');
+  });
+
+  it('should return string with 10 characters if word longer than 10', () => {
+    expect(component.transform('1234567890123456')).toBe('1234567890...');
+  });
+
+  it('should return string with the exact number of characters if requested', () => {
+    expect(component.transform('123456', 2)).toBe('12...');
+  });
+
+  it('should return the input word if shorter than 10 chars', () => {
+    expect(component.transform('1234')).toBe('1234');
   });
 });
